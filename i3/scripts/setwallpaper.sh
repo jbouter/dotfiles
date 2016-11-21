809354dc388abfd4c7baf5ed17d87f3480925526
Jeffrey Bouter <jb@warpnet.nl>
2016-11-21 01:26:38 +0100

    Updated wallpaper, goodnight.

--- i3/scripts/setwallpaper.sh
+++ i3/scripts/setwallpaper.sh
@@ -8,7 +8,7 @@ fi
 
 
 # Set the wallpaper
-DISPLAY=$SETDISPLAY feh --bg-fill "/home/jeffrey/documents/nextcloud/wallpapers/himmel.jpg"
+DISPLAY=$SETDISPLAY feh --bg-fill "/home/jeffrey/documents/nextcloud/wallpapers/dragonfly_ultraviolet.png"
 
 # Set screen blanking to 1hr
 DISPLAY=$SETDISPLAY xset s 3600 3600 &
