913ebd3a4ab492f1c74767f66d94fc8735879234
Jeffrey Bouter <jb@warpnet.nl>
2016-12-15 10:09:15 +0100

    Added i3-tranquil, specific for xps13

---
+++ i3-tranquil/scripts/setwallpaper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -f ~/.config/i3/conf.d/display.txt ]; then
+  SETDISPLAY=$(cat ~/.config/i3/conf.d/display.txt)
+else
+  SETDISPLAY=":0.0"
+fi
+
+
+# Set the wallpaper
+DISPLAY=$SETDISPLAY feh --bg-fill "/home/jeffrey/documents/nextcloud/wallpapers/0aXV7W3.jpg"
+
+# Set screen blanking to 1hr
+DISPLAY=$SETDISPLAY xset s 3600 3600 &
